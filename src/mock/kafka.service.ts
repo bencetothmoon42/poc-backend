5f0581e94c631ebf494fbb2153e156fb2122c8c7
mnzl <matze@silpion.de>
2022-10-10 15:02:29 +0200

    proper termination

--- src/mock/kafka.service.ts
+++ src/mock/kafka.service.ts
@@ -10,11 +10,13 @@ export class MockKafkaService {
 
 	private readonly logger = new Logger(MockKafkaService.name);
 	private ids =  [];
+	private running = false;
 
 	async onModuleInit() {
 		//create a testing status stream generator
 		this.logger.log("mocked kafka generator starting");
 		//start infinite producer loop
+		this.running = true;
 		this.run();
 	}
 
@@ -22,8 +24,8 @@ export class MockKafkaService {
 		this.eventEmitter.emit('message.rcvd', msg);
 	}
 	
-	async *printerStatus(): AsyncGenerator<{id:string, status:string}, never>{
-		while(true){
+	async *printerStatus(): AsyncGenerator<{id:string, status:string}, void>{
+		while(this.running){
 			await new Promise(resolve => setTimeout(resolve, 1000));
 			yield {id: crypto.randomUUID(), status: 'ok'};
 		}
@@ -34,6 +36,12 @@ export class MockKafkaService {
 			this.logger.log(`produced ${JSON.stringify(status)}`);
 			this.consume(status);
 		};
+	}
+
+	async onModuleDestroy(){
+		//cleanup&terminate
+		this.logger.log('terminating');
+		this.running = false;
 	}
 
 
