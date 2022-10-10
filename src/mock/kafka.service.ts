c18fcd6c833aa9bab665082cb006aa8ca8e0c2b9
mnzl <matze@silpion.de>
2022-10-10 12:56:54 +0200

    kafka module

---
+++ src/mock/kafka.service.ts
@@ -0,0 +1,48 @@
+import { Logger, Injectable, OnModuleInit, INestApplication } from '@nestjs/common';
+import { OnEvent, EventEmitter2 } from '@nestjs/event-emitter';
+
+@Injectable()
+export class MockKafkaService {
+
+	constructor(private eventEmitter: EventEmitter2){};
+
+	private readonly logger = new Logger(MockKafkaService.name);
+	private ids =  [];
+
+	async onModuleInit() {
+		//create a testing status stream generator
+		this.logger.log("mocked kafka generator starting");
+		//start infinite producer loop
+		this.run();
+	}
+
+	consume(msg){
+		this.eventEmitter.emit('message.rcvd', msg);
+	}
+	
+	async *printerStatus(): AsyncGenerator<{id:string, status:string}, never>{
+		while(true){
+			await new Promise(resolve => setTimeout(resolve, 1000));
+			yield {id: "1", status: 'ok'};
+		}
+	}
+
+	async run(){
+		for await (let status of this.printerStatus()) {
+			this.logger.log("produced "+status);
+			this.consume(status);
+		};
+	}
+
+
+}
+
+
+
+
+//REMOVEME: infinite event generator for testing 
+//REMOVEME: ugly testing shit
+
+
+
+
